eb36cf10aae3913a7a2255b5ebe903d4f4115cf9
Yaroslav <y-panteleew@yandex.ru>
2023-02-02 23:17:30 +0400

    fix: creators list random, favicon and min-heignt header

--- src/widgets/about/index.tsx
+++ src/widgets/about/index.tsx
@@ -5,17 +5,37 @@ import { Preloader } from '../../shared/ui/preloader';
 import styles from './styles.module.scss';
 import { AboutListItem } from './ui/listitem';
 
+function shuffle(array: typeof creators) {
+  let currentIndex = array.length;
+  let randomIndex;
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+const creators = [
+  <AboutListItem title="Illustration" descr="Sofia Kuzina" />,
+  <AboutListItem title="Motion Design" descr="Olga Khil" />,
+  <AboutListItem title="Developer" descr="Yaroslav Panteleev" />,
+  <AboutListItem title="Product Design" descr="Artem Mukhammedzhanov" />,
+];
+
 export const About = () => {
+  const usArr = shuffle(creators);
   return (
     <Container>
       <div className={styles.character}>
         <Preloader className={styles.animCharacter} viewBox="0 0 650 650" />
       </div>
       <div className={styles.team}>
-        <AboutListItem title="Illustration" descr="Sofia Kuzina" />
-        <AboutListItem title="Motion Design" descr="Olga Khil" />
-        <AboutListItem title="Developer" descr="Yaroslav Panteleev" />
-        <AboutListItem title="Product Design" descr="Artem Mukhammedzhanov" />
+        {
+          creators.map((item) => item)
+        }
       </div>
     </Container>
   );
